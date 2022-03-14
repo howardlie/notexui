2890d3c927ad140d4c181d7b906c26084d21f6e7
Howard <liehoward13@gmail.com>
2022-03-14 23:06:33 +0700

    added uuid lib

--- src/app/note.service.ts
+++ src/app/note.service.ts
@@ -1,12 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
 
-  
+  public mynotes = null;
 
-  constructor() { }
+  public sharednotes = null;
+
+  public deletednotes = null;
+
+  public archivednotes = null;
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  sync() {
+
+  }
+
+  openSharedNotes(id: string) {
+
+  }
+
+
 
 }
