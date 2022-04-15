f2af6acf3f5f76e8bc6142e66639bf4f2e76bdb1
Howard <liehoward13@gmail.com>
2022-04-15 22:52:14 +0700

    added reminder service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ReminderService } from './reminder.service';
 import { DeviceService } from './device.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
@@ -8,18 +9,23 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { DeviceListComponent } from './device-list/device-list.component';
 import { AlertController } from '@ionic/angular';
+import { SwPush } from '@angular/service-worker';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
+
+  readonly VAPID_PUBLIC_KEY = "BKHMaad8FNpOGnxEqxhj5nhfzGTollnuVvYcMU_e1B7rYCAEKgVB8B_mRIUlaZFmVHvJ1XLg2uVT2OVW6dxkmY4";
+
   socialUser!: SocialUser;
   isLoggedin?: boolean;
   isOnline: boolean;
-  constructor(public modalController: ModalController, public alertController: AlertController, private socialAuthService: SocialAuthService, public authService: AuthService, private http: HttpClient, private deviceService: DeviceService) {}
+  constructor(private swPush: SwPush, private reminderService: ReminderService, public modalController: ModalController, public alertController: AlertController, private socialAuthService: SocialAuthService, public authService: AuthService, private http: HttpClient, private deviceService: DeviceService) {}
 
   ngOnInit() {
+
     this.socialAuthService.authState.subscribe((user) => {
       this.socialUser = user;
       this.authService.loginUser(this.socialUser);
@@ -32,6 +38,15 @@ export class AppComponent implements OnInit {
     });
     this.deviceService.startRefresh();
   }
+/*
+  subscribeToNotifications() {
+
+    this.swPush.requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+    .then(sub => this.reminderService.addPushSubscriber(sub))
+    .catch(err => console.error("Could not subscribe to notifications", err));
+  }*/
 
   async presentDevicesModal() {
     const modal = await this.modalController.create({
@@ -39,6 +54,8 @@ export class AppComponent implements OnInit {
     });
     return await modal.present();
   }
+
+  
 
   async presentAlertConfirmLogout() {
     const alert = await this.alertController.create({
